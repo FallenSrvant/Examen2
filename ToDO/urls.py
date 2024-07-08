89e15134f306f7fe1732e8203a8ffb67f20cc23e
FallenSrvant <119888029+FallenSrvant@users.noreply.github.com>
2024-07-07 17:57:36 -0700

    Update urls.py

--- ToDO/urls.py
+++ ToDO/urls.py
@@ -5,6 +5,10 @@ from ToDO import views
 
 router = routers.DefaultRouter()
 router.register(r'ToDO',views.ToDOView,'ToDO')
+router.register(r'ToDO-ids', views.ToDOIDListView, 'ToDO-ids')
+router.register(r'ToDO-id-titles', views.ToDOIdTitleListView, 'ToDO-id-titles')
+router.register(r'ToDO-unresolved', views.ToDOUnresolvedView, 'ToDO-unresolved')
+router.register(r'ToDO-resolved', views.ToDOResolvedView, 'ToDO-resolved')
 # API version
 urlpatterns = [
     path("api/v1/", include(router.urls)),
