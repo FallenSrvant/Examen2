7e90e4d622cbcd96dc7a1a69f69af0bb2ff67001
FallenSrvant <119888029+FallenSrvant@users.noreply.github.com>
2024-07-07 17:57:03 -0700

    Update views.py

--- ToDO/views.py
+++ ToDO/views.py
@@ -1,8 +1,24 @@
 from rest_framework import viewsets, generics
-from .serializer import toDOSerializer
+from .serializer import toDOSerializer,ToDOIDSerializer, ToDOIdTitleSerializer
 from .models import ToDO
 # Create your views here.
 
 class ToDOView(viewsets.ModelViewSet):
     serializer_class = toDOSerializer
     queryset = ToDO.objects.all()
+
+class ToDOIDListView(viewsets.ModelViewSet):
+    serializer_class = ToDOIDSerializer
+    queryset = ToDO.objects.all()
+
+class ToDOIdTitleListView(viewsets.ModelViewSet):
+    serializer_class = ToDOIdTitleSerializer
+    queryset = ToDO.objects.all()
+
+class ToDOUnresolvedView(viewsets.ModelViewSet):
+    serializer_class = toDOSerializer
+    queryset = ToDO.objects.filter(done=False)
+
+class ToDOResolvedView(viewsets.ModelViewSet):
+    serializer_class = toDOSerializer
+    queryset = ToDO.objects.filter(done=True)
